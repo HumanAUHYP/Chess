36e1ebf88d6fd717a66ef2f6adead233e2b601e5
humanplay228@gmail.com <humanplay228@gmail.com>
2021-06-02 13:20:47 +0300

    Delete and move

--- Chess4/MainWindow.xaml.cs
+++ Chess4/MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace Chess4
     /// </summary>
     public partial class MainWindow : Window
     {
-        public bool addOrMove = false;
+        public bool add = false;
+        public bool move = false;
         public string pieceName;
         
         public MainWindow()
@@ -31,17 +32,39 @@ namespace Chess4
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            addOrMove = true;
+            add = true;
+            btnAdd.Background = Brushes.Green;
         }
 
         private void Cell_Click(object sender, RoutedEventArgs e)
         {
-           (sender as Button).Content = pieceName;
+            if (add)
+            {
+                (sender as Button).Content = pieceName;
+                add = false;
+                btnAdd.Background = Brushes.Red;
+            }
+            else if(move)
+            {
+                (sender as Button).Content = pieceName;
+                move = false;
+            }
+            else if((sender as Button).Content != null)
+            {
+                pieceName = Convert.ToString((sender as Button).Content);
+                (sender as Button).Content = null;
+                move = true;
+            }
         }
 
         private void PieceName_Click(object sender, RoutedEventArgs e)
         {
             pieceName = Convert.ToString((sender as Button).Content);
+        }
+
+        private void Cell_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            (sender as Button).Content = null;
         }
     }
 }
